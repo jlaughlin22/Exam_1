c488cd788f378f4ec9ab064e61013f996a01e597
jlaughlin22 <46356607+jlaughlin22@users.noreply.github.com>
2019-06-06 11:40:54 -0700

    Added all files to folder

---
+++ source/Sedan.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////
+// Exam 1
+//CS273
+//Author: Jonathan Laughlin
+//Date created: 6/6/19
+//Date Last Modified: 6/6/19
+//Professor: Pete Tucker
+// This code contains my code for Exam 1 for CS273
+///////////////////////////////////////////////////////
+
+//I affirm that all code given below was written solely by me, <Jonathan Laughlin>, and that any help I received adhered to the rules stated for this exam.
